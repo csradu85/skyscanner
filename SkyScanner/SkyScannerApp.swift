bbfd60bef58da78024b56d0c458622c1f6a6efc3
cradu <cradu@csrinnov.co.za>
2026-01-21 10:59:49 +0200

    first commit

---
+++ SkyScanner/SkyScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyScannerApp.swift
+//  SkyScanner
+//
+//  Created by cradu on 2026/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SkyScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkyScannerTests/SkyScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkyScannerTests.swift
+//  SkyScannerTests
+//
+//  Created by cradu on 2026/01/21.
+//
+
+import Testing
+@testable import SkyScanner
+
+struct SkyScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
